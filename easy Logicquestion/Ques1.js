e00da2472387717551747a5f9837b5ff81f01b14
Gursahib Singh <aulakhg808@gmail.com>
2025-06-06 11:23:09 +0530

    solved question of logic building

---
+++ easy Logicquestion/Ques1.js
@@ -0,0 +1,22 @@
+const user = [{
+    name : "sahib",
+    email : "sahib@gmail.com",
+    age : 21 
+},{
+    name : "nav",
+    email : "nav@gmail.com",
+    age : 24
+},{
+    name : "raaaj",
+    email : "raaaj@gmail.com",
+    age : 20
+}, {
+    name : "deep",
+    email : "deep@gmail.com",
+    age : 16
+}]
+
+
+function updateemail (users , name , newemail){
+    const data = users.find( user =>user.name === name);
+}
