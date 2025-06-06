c34962d57307be8d9317ffbae1ca8292764d1ffb
Gursahib Singh <aulakhg808@gmail.com>
2025-06-06 23:05:26 +0530

    js logicbuilding questions

--- easy Logicquestion/Ques1.js
+++ easy Logicquestion/Ques1.js
@@ -1,22 +1,68 @@
-const user = [{
-    name : "sahib",
-    email : "sahib@gmail.com",
-    age : 21 
-},{
-    name : "nav",
-    email : "nav@gmail.com",
-    age : 24
-},{
-    name : "raaaj",
-    email : "raaaj@gmail.com",
-    age : 20
-}, {
-    name : "deep",
-    email : "deep@gmail.com",
-    age : 16
-}]
+const inventory = [ "apple", "bannana" , "orange" , "milk", "bread"];
 
-
-function updateemail (users , name , newemail){
-    const data = users.find( user =>user.name === name);
+function checkproduct(inventory , produnctname){
+    return inventory.includes(produnctname)
 }
+
+// console.log(checkproduct(inventory,"milk"));
+
+// 2 question
+// User Profile (Object)
+//  You’re building a profile system. Each user’s profile is stored as an object with name, email, and age properties. Write a function to update a user’s email address when given their name.
+
+const profile  = [ 
+    {
+        name: "sahib",
+        age : 21,
+        email : "sahib@gmail.com"
+    },
+    {
+        name : "raaaj",
+        age : 20,
+        email : "raaaj@gmail.com"
+    },
+    {
+        name : "nav", 
+        age : 26,
+        email : "nav@gmail.com"
+    }
+]
+
+function updateemail (profile , name , newemail){
+    for( let user of profile){
+        if (user.name === name) {
+            user.email = newemail;
+            return true
+        }
+    }
+    return false;
+}
+
+// console.log(updateemail(profile,"nav" , "nav200@gmail.com"));
+// console.log(profile);
+
+const list  = [ {
+    name : "bathing",
+    status : "done"
+},{
+    name : "washing",
+    status : "pending"
+},{
+    name : "breakfast",
+    status : "done"
+},{
+    name : "reached",
+    status : "pending"
+},
+];
+
+function countpending (task){
+    let count = 0; 
+    for(let tasks of task){
+        if(tasks.status === "pending"){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countpending(`these item currently pending : ${list}`))
